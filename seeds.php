4a6b11e888ba16567326fab140dd0ff7b6f3aeec
douglasSalvagni <douglassalvagni@gmail.com>
2022-11-07 08:34:07 -0300

    seeds

--- seeds.php
+++ seeds.php
@@ -6,8 +6,15 @@
 try {
     $db = new PDO("sqlite:base.sqlite");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Error Handling
-    $sql = "INSERT INTO users (username, password) VALUES ('John', 'teste')";
+    // $sql = "INSERT INTO users (userid, username, password) VALUES ('1', 'John', 'teste')";
+    // $db->exec($sql);
+
+    
+    $sql = "INSERT INTO user_permissions (pid, permission_name, permission_type, userid) VALUES ('1', 'page_dashboard', '1', '1')";
     $db->exec($sql);
+
+
+
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
